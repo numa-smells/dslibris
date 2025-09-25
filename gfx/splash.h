73c81fd1399c6a57fc250915cb4a713a689a50b5
Adrian Gjonca <gjoncaadrian@gmail.com>
2025-09-25 23:04:49 +0100

    Another (better) splash

--- gfx/splash.h
+++ gfx/splash.h
@@ -6,9 +6,9 @@
 //	splash, 256x256@16, 
 //	Alphabit on.
 //	+ bitmap lz77 compressed
-//	Total size: 19932 = 19932
+//	Total size: 33284 = 33284
 //
-//	Time-stamp: 2025-09-25, 03:19:20
+//	Time-stamp: 2025-09-25, 23:02:59
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_SPLASH_H
 #define GRIT_SPLASH_H
 
-#define splashBitmapLen 19932
-extern const unsigned int splashBitmap[4983];
+#define splashBitmapLen 33284
+extern const unsigned int splashBitmap[8321];
 
 #endif // GRIT_SPLASH_H
 
